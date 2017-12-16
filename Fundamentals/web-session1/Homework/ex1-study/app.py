5fb43806447c1722f53fd8f6c917df4d365ca3c9
Pham Thanh Dat <ptd2903@gmail.com>
2017-12-16 17:13:02 +0700

    web1

---
+++ Fundamentals/web-session1/Homework/ex1-study/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
+
+
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html')
+
+@app.route('/school')
+def school():
+    return redirect('http://techkids.vn', code = 302)
+
+if __name__ == '__main__':
+  app.run(debug=True)
---
+++ Fundamentals/web-session1/Homework/ex1/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>Body Mass Index</h1>"
+
+@app.route('/bmiwithoutrender/<weight>/<height>')
+def bmiwithoutrender(weight, height):
+    w = int(weight)
+    h = float(height)
+    bmi = w/(h**2)
+    if bmi < 16:
+        result = 'Severely underweight.'
+    elif 16 <= bmi < 18.5:
+        result = 'Underweight.'
+    elif 18.5 <= bmi < 25:
+        result = 'Normal.'
+    elif 25 <= bmi < 30:
+        result = 'Overweight.'
+    else:
+        result = 'Obese.'
+    return '<p>Your weight:</p>' + str(w) + '<p>Your height:</p>' + str(h) + '<p>Your BMI:</p>' + str(bmi) + '<p>Result:</p>' + result
+
+@app.route('/bmiwithrender/<weight>/<height>')
+def bmiwithrender(weight, height):
+    w = int(weight)
+    h = float(height)
+    bmi = w/(h**2)
+    return render_template('bmi.html', w = w, h = h, bmi = bmi)
+
+if __name__ == '__main__':
+  app.run(debug=True)
