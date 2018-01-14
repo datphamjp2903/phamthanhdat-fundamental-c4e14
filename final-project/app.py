ea6bfc1b5216bcc56ae6d7289958ffb6460dc3c6
HungDS <hungds1996@gmail.com>
2018-01-14 12:29:38 +0700

    Update app.py

--- final-project/app.py
+++ final-project/app.py
@@ -1,14 +1,5 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/english')
-def english():
-    return render_template('english-center-list.html')
-
 if __name__ == '__main__':
   app.run(debug=True)
